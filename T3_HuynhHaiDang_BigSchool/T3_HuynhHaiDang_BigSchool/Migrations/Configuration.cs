32b58e28cf65d594fde653654518c3f308429238
dangharold <90547689+dangharold@users.noreply.github.com>
2022-03-23 17:21:50 +0700

    Commit 17:21 23/03/2022

---
+++ T3_HuynhHaiDang_BigSchool/T3_HuynhHaiDang_BigSchool/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace T3_HuynhHaiDang_BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<T3_HuynhHaiDang_BigSchool.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "T3_HuynhHaiDang_BigSchool.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(T3_HuynhHaiDang_BigSchool.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ T3_HuynhHaiDang_BigSchool/T3_HuynhHaiDang_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(T3_HuynhHaiDang_BigSchool.Startup))]
+namespace T3_HuynhHaiDang_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
