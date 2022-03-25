1073d368fc8b0ff1418a5c18d3114fdf5e524039
dangharold <90547689+dangharold@users.noreply.github.com>
2022-03-25 17:21:51 +0700

    Commit luc 17:21 25/03/2022

--- 1911065192_HuynhHaiDang_BigSchool/1911065192_HuynhHaiDang_BigSchool/Controllers/HomeController.cs
+++ 1911065192_HuynhHaiDang_BigSchool/1911065192_HuynhHaiDang_BigSchool/Controllers/HomeController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+using _1911065192_HuynhHaiDang_BigSchool.Models;
+using _1911065192_HuynhHaiDang_BigSchool.ViewModels;
 
 namespace _1911065192_HuynhHaiDang_BigSchool.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private ApplicationDbContext_dbContext _dbContext;
+        public HomeController()
         {
-            return View();
+            _dbContext = new ApplicationDbContext();
+        }
+            public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var upcommingCourses = _dbContext.Courses.Include(c => c.Lecturer).Include(c => c.Category).Where(c => c.DateTime > DateTime.Now);
+           return View(upcommingCourses);
+           
         }
 
         public ActionResult About()
