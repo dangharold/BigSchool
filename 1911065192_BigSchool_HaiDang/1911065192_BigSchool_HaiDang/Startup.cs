ffcf7b36ee528624a7d85957e6d0040e63cc45a7
dangharold <90547689+dangharold@users.noreply.github.com>
2022-03-28 02:36:21 +0700

    commit 2:36

---
+++ 1911065192_BigSchool_HaiDang/1911065192_BigSchool_HaiDang/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1911065192_BigSchool_HaiDang.Startup))]
+namespace _1911065192_BigSchool_HaiDang
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
